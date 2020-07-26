047e97146cd844f167da78c2e2f2195be1003261
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2020-07-26 02:38:36 -0300

    se agregaron entradas para JS y HTML

---
+++ src/js/admin.js
@@ -0,0 +1,5 @@
+let nombre="carlos";
+
+
+
+console.log(nombre);
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '../css/style.css'
+
+
+let comision = "7D";
+console.log(comision);
