fd58dd9b7fda92f1baac3555a668bec118e9c877
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2020-07-26 02:56:24 -0300

    se agrego la configuracion de imagenes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
  
 module.exports = {
@@ -18,7 +19,19 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                name: 'img/[name].[ext]',
+             },
+          },
+        ],
       }
+
 
     ]
   },
@@ -53,7 +66,18 @@ module.exports = {
         }),
     new MiniCssExtractPlugin({
         filename: 'css/style.css'
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: './src/img',
+            to: 'img',
+          },
+         
+        ]
     })
+
+
 
 ],
 
