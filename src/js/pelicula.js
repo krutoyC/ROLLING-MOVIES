2ec6d1f660a84ecf468003ae6b1b883fc7b9d111
Tomas Quiroga <quirogatomas30@gmail.com>
2020-07-30 15:53:51 -0300

    se agrego la logica para guardar nuevas peliculas y series

---
+++ src/js/pelicula.js
@@ -0,0 +1,12 @@
+export default class Pelicula{
+    constructor(codigo, nombre, tipo, categoria, descripcion, publicado, imagen, trailer){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.publicado = publicado;
+        this.imagen = imagen;
+        this.trailer = trailer;
+    }
+}
