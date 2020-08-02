df5bc09bb5a9dd80c4934c4ed2ff7e35714db818
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2020-08-02 19:37:31 -0300

    se agrego Emailjs y un boton de alerta para confirmar el envio

--- src/js/Contacto.js
+++ src/js/Contacto.js
@@ -61,10 +61,61 @@ window.validartodo = function (event) {
     console.log("validar todo");
     //tratar de que todo este validado
     if (revisar(document.getElementById("nombre")) && revisartextarea(document.getElementById("textArea")) && validarEmail(document.getElementById("email")) && validartilde()) {
+        enviarEmail();
         alert("Consulta lista para ser enviada");
     } else {
         alert("Hay un error en el formulario");
     }
 
 }
+
+function enviarEmail() {
+    
+    
+    let template_params = {
+        from_name: document.getElementById("nombre").value,
+       
+        message_html: `Mensaje: ${document.getElementById("textArea").value} - Email: ${document.getElementById("email").value}`
+    };
+    
+    let service_id = "default_service";
+    let template_id = "template_P80i089q";
+    emailjs.send(service_id, template_id, template_params).then(function(response){
+        console.log("se envio correctamente"+response);
+        document.getElementById("envio").className="alert alert-primary my-4";
+document.getElementById("envio").innerText="Su consulta fué enviada de forma correcta";
+    },function (error){
+        console.log("fallo en el envio"+error);
+        ocument.getElementById("envio").className="alert alert-danger my-4";
+document.getElementById("envio").innerText="Falló en el envio";
+    })
+    
+}
+
+
+// function enviarEmail(){
+//     let template_params = {
+        
+//         from_name: document.getElementById("nombre").value,
+        
+//         message_html: `Mensaje: ${document.getElementById("textArea").value} - Email: ${document.getElementById("email").value}`
+// //      };
+//      }
+     
+//      let service_id = "default_service";
+//      let template_id = "template_UMDsDNDO";
+//      emailjs.send(service_id, template_id, template_params).then(
+//          function(response){
+//              console.log("se envio correctamente"+response);
+//              document.getElementById("envio").className="alert alert-primary my-4";
+// document.getElementById("envio").innerText="Su consulta fué enviada de forma correcta";
+//          },function (error){
+//              console.log("fallo en el envio"+error);
+//              ocument.getElementById("envio").className="alert alert-danger my-4";
+// document.getElementById("envio").innerText="Falló en el envio";
+//          }
+             
+         
+//      )
+// }
 
