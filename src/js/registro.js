da0412aee0232c294936042948acfdccc24b9f8d
Jose Ignacio Lopez <joseignalopez@gmail.com>
2020-08-02 12:26:11 -0300

    cambios varios

---
+++ src/js/registro.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all.min.js'
+import '../css/style.css';
+
+console.log("desde registro")
