86be7a732faed1e00552234a2986b085bb3381a6
Barbara <mahara14@hotmail.com>
2020-08-06 00:06:18 -0300

    funcion buscar en la pagina index

--- src/js/acercaDeNosotros.js
+++ src/js/acercaDeNosotros.js
@@ -2,18 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.min.js'
 import '../css/style.css';
-console.log("eacerca de nosotros");
+console.log("acerca de nosotros");
 
 let buscar = document.querySelector(`#buscar`);
 let botonBuscar = document.querySelector(`#botonBuscar`);
 let resultado = document.querySelector(`#resultados`);
 
-let productos = [
-    {codigo: `2233`, nombre: `luffy`},
-    {codigo: `1234`, nombre: `Naruto`},
-    {codigo: 5677, nombre: `Alice`, tipo: `serie`, categoria: `accion`, actores: `alicia Manuela` },
-    {codigo: 7890, nombre: `Anne with e`, tipo: `serie`, categoria: `accion`, actores: `Anita L` },
-]
+let peliculas = JSON.parse(localStorage.getItem(`peliculaKey`));
 
 window.filtrar = function (){
     console.log(buscar.value);
@@ -21,18 +16,30 @@ window.filtrar = function (){
     let texto = buscar.value.toLowerCase();
    
 
-    for(let producto of productos){
-        let nombre = producto.nombre.toLocaleLowerCase();
+    for(let pelicula of peliculas){
+        let nombre = pelicula.nombre.toLocaleLowerCase();
         if(nombre.indexOf(texto) !== -1){
-            resultado.innerHTML += `<li class= "text-light"><a class="text-decoration-none" href="info.html"> Nombre: ${producto.nombre} - Tipo: ${producto.tipo} - Categoria: ${producto.categoria}</a></li>`
+            resultado.innerHTML += `<div class="card-deck col-sm-12 col-md-6 col-lg-3 my-3">
+            <div class="card cardGrow">
+            <a class="text-decoration-none text-light" href="infoSeries.html?search=${pelicula.codigo}">
+            <img src="img/${pelicula.imagen}" class="card-img-top" alt="${pelicula.nombre}">
+            <h5 class="card-title text-center mt-2">${pelicula.nombre}</h5>
+            </div></a></div>`
+            
+            //`<li class= "text-light"><a class="text-decoration-none" href="info.html"> Nombre: ${pelicula.nombre} - Tipo: ${pelicula.tipo} - Categoria: ${pelicula.categoria}</a></li>`
         }
     }
     
-
     if(resultado.innerHTML === ``){
-        resultado.innerHTML += `<li class= "font-weight-bold text-light"> Película o Serie no encontrada </li>`
+        resultado.innerHTML +=  `<li class= "font-weight-bold text-light"> Película o Serie no encontrada </li>`
     }
 }
 
 botonBuscar.addEventListener(`click`,filtrar);
-//buscar.addEventListener(`keyup`,filtrar)
+buscar.addEventListener(`keyup`, function (event){
+    if (event.keyCode === 13) { 
+        event.preventDefault();
+        console.log(event);
+       }
+
+});
