ef14b600628647018627f686522aa2c9eb0c20e8
Barbara <mahara14@hotmail.com>
2020-08-05 22:56:41 -0300

    prueba buscador

--- src/js/acercaDeNosotros.js
+++ src/js/acercaDeNosotros.js
@@ -3,3 +3,36 @@ import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.min.js'
 import '../css/style.css';
 console.log("eacerca de nosotros");
+
+let buscar = document.querySelector(`#buscar`);
+let botonBuscar = document.querySelector(`#botonBuscar`);
+let resultado = document.querySelector(`#resultados`);
+
+let productos = [
+    {codigo: `2233`, nombre: `luffy`},
+    {codigo: `1234`, nombre: `Naruto`},
+    {codigo: 5677, nombre: `Alice`, tipo: `serie`, categoria: `accion`, actores: `alicia Manuela` },
+    {codigo: 7890, nombre: `Anne with e`, tipo: `serie`, categoria: `accion`, actores: `Anita L` },
+]
+
+window.filtrar = function (){
+    console.log(buscar.value);
+    resultado.innerHTML = ``;
+    let texto = buscar.value.toLowerCase();
+   
+
+    for(let producto of productos){
+        let nombre = producto.nombre.toLocaleLowerCase();
+        if(nombre.indexOf(texto) !== -1){
+            resultado.innerHTML += `<li class= "text-light"><a class="text-decoration-none" href="info.html"> Nombre: ${producto.nombre} - Tipo: ${producto.tipo} - Categoria: ${producto.categoria}</a></li>`
+        }
+    }
+    
+
+    if(resultado.innerHTML === ``){
+        resultado.innerHTML += `<li class= "font-weight-bold text-light"> Película o Serie no encontrada </li>`
+    }
+}
+
+botonBuscar.addEventListener(`click`,filtrar);
+//buscar.addEventListener(`keyup`,filtrar)
